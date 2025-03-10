071ca0968c38be205745d1945528d6ad97413f39
hmmueller <harald_m_mueller@gmx.de>
2025-03-10 19:48:03 +0100

    i18n

---
+++ PathGCodeLibrary/AbstractOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace de.hmmueller.PathGCodeLibrary;
+
+public class AbstractOptions {
+    protected static FormatException FormatException(string message, params object[] pars)
+        => new FormatException(string.Format(message, pars));
+
+    protected static string GetStringOption3(string[] args, ref int i, string missingMessage) {
+        return args[i].Length > 2 ? args[i][2..]
+            : i < args.Length - 1 ? args[++i]
+            : throw FormatException(missingMessage, args[i - 1]);
+    }
+
+
+}
+
